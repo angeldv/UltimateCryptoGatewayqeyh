08d7106166b368971660de89459ab2c70484f6fe
angeldv <angeldv@eteb.me>
2025-07-09 18:04:13 +0700

    Add ultimatecryptogatewayqeyh/__init__.py

---
+++ ultimatecryptogatewayqeyh/__init__.py
@@ -0,0 +1,17 @@
+# ultimatecryptogatewayqeyh/__init__.py
+"""
+UltimateCryptoGatewayqeyh - A powerful utility for data processing.
+"""
+
+__version__ = "0.1.0"
+__author__ = "angeldv"
+__license__ = "MIT"
+
+from .core import Processor
+from .utils import (
+    load_data,
+    save_data,
+    transform,
+    validate_email,
+    generate_hash
+)
